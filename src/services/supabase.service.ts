b2afa6cfc88115eca9307513375edae8dbf11b5c
Felipe Nelli Martins <Fn.martins@outlook.com>
2024-10-23 03:15:26 -0300

    Add supabase + prisma service

---
+++ src/services/supabase.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient } from '@supabase/supabase-js';
+
+@Injectable()
+export class SupabaseService {
+    constructor(private configService: ConfigService) {}
+    
+    private supabaseUrl = this.configService.get<string>('SUPABASE_URL')
+    private supabaseAnonKey = this.configService.get<string>('SUPABASE_ANON_KEY')
+    
+    private supabase = createClient(this.supabaseUrl!, this.supabaseAnonKey!, {});
+
+    getSupabase() {
+        return this.supabase
+    }
+}
