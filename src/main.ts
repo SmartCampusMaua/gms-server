48689eedc612fb65de12f144dea6b320e9b3478f
Felipe Nelli Martins <Fn.martins@outlook.com>
2024-10-24 01:14:29 -0300

    Changed how .env data is obtained

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const gmsServerPort = configService.get<string>('GMS_SERVER_PORT')!;
   
-  await app.listen(3002);
+  await app.listen(gmsServerPort);
 }
 bootstrap();
